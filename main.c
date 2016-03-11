96a3ee0a4bc87d4450d3fb161360b12c942057d0
Admin <detect1ve@ubuntu.ubuntu-domain>
2016-03-12 02:22:07 +0900

    Search roots

--- main.c
+++ main.c
@@ -3,9 +3,20 @@
 
 int main()
 {
-	int a, b, c, d;
+	int a, b, c, d, x1, x2;
+
 	printf("Input value: ");
 	scanf("%d%d%d", &a, &b, &c);
 	d = b * b - 4 * a * c;
+	if(x >= 0)
+	{
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+	}
+	if(x < 0)
+	{
+		puts("Negative discriminant");
+		return 0;
+	}
 	return 0;
 }
