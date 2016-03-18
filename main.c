95545e8885386683817efb6c0d82cdfea354678d
Admin <detect1ve@ubuntu.ubuntu-domain>
2016-03-18 19:02:47 +0600

    Error correction after merge:

--- main.c
+++ main.c
@@ -3,21 +3,21 @@
 
 int main()
 {
-	int a, b, c, d, x1, x2;
+	float a, b, c, d, x1, x2;
 
 	printf("Input value: ");
-	scanf("%d%d%d", &a, &b, &c);
+	scanf("%f%f%f", &a, &b, &c);
 	d = b * b - 4 * a * c;
-	if(d >= 0)
+	if (d < 0)
+        {
+                puts("Negative discriminant");
+                return 0;
+        }
+	if (d >= 0)
 	{
 		x1 = (-b + sqrt(d)) / (2 * a);
 		x2 = (-b - sqrt(d)) / (2 * a);
 	}
-	if(d < 0)
-	{
-		puts("Negative discriminant");
-		return 0;
-	}
-	printf("x1 = %d, x2 = %d\n", x1, x2);
+	printf("x1 = %.6f, x2 = %.6f\n", x1, x2);
 	return 0;
 }
